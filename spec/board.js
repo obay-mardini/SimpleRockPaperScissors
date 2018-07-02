ccd536ef89fdd0daac17073230e53f4722b9c9a2
obay <obaymardini@gmail.com>
2018-07-02 20:13:04 +0200

    Fix unit tests for board.js

--- spec/board.js
+++ spec/board.js
@@ -1,18 +1,23 @@
 describe.only("board", () => {
-  const players = ["obay", "computer"];
+  const players = [{ name: "obay" }, { name: "computer" }];
   let mockedPlayer;
+  let resetGameVisualsStub;
   this.clock = sinon.useFakeTimers();
 
   beforeEach(() => {
+    resetGameVisualsStub = sinon.stub(Board.prototype, "resetGameVisuals");
     mockedPlayer = sinon.stub(window, "Player");
   });
   afterEach(() => {
     mockedPlayer.restore();
+    resetGameVisualsStub.restore();
   });
 
   it("should create a board with any number players instances correctly", () => {
+    const startTimerStub = sinon.stub(Board.prototype, "startTimer");
     const board = new Board(...players, "thirdPlayer");
     expect(board.players.length).equal(3);
+    startTimerStub.restore();
   });
 
   it("should check the result at the end of each timer", () => {
@@ -23,6 +28,7 @@ describe.only("board", () => {
     expect(checkResultsStub.called).equal(true);
     checkResultsStub.restore();
   });
+
   it("should call showWinner at the end of the third round", () => {
     const board = new Board(...players);
     const showWinnerSpy = sinon.stub(board, "showWinner");
@@ -36,31 +42,25 @@ describe.only("board", () => {
     showWinnerSpy.restore();
   });
 
-  it("should set the first player as winner when its score is higer than the second", () => {
-    mockedPlayer
-      .onFirstCall()
-      .returns({ score: 2 })
-      .onSecondCall()
-      .returns({ score: 1 });
-    const board = new Board(...players);
+  it.only("should set the first player as winner when its score is higer than the second", () => {
+    const board = new Board(
+      { name: players[0], score: 2 },
+      { name: players[1], score: 1 }
+    );
     board.showWinner();
-    console.log(board.winner);
-    expect(board.winner.score).equal(2);
+    expect(board.winner.name).equal(players[0].name);
   });
 
   it("should set the second player as winner when its score is higer than the first", () => {
-    mockedPlayer
-      .onFirstCall()
-      .returns({ score: 1 })
-      .onSecondCall()
-      .returns({ score: 2 });
-    const board = new Board(...players);
+    const board = new Board(
+      { name: players[0], score: 1 },
+      { name: players[1], score: 2 }
+    );
     board.showWinner();
-    console.log(board.winner);
-    expect(board.winner.score).equal(2);
+    expect(board.winner.name).equal(players[1].name);
   });
 
-  it("should set empty object to winner property when tie", () => {
+  it("should set empty object to winner prplayers[1]operty when tie", () => {
     mockedPlayer
       .onFirstCall()
       .returns({ score: 1 })
